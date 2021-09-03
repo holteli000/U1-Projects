4249008ae2190bacaa717e15585224aac1a5efe6
holteli000 <holteli000@warren.k12.in.us>
2021-09-03 13:22:31 -0400

    u1 projects

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,6 +1,42 @@
-public class MadLib {
+  import java.util.Scanner;
 
-    /*Create your own MadLibs. (https://www.madtakes.com/) You may use a story or example from the
-    website if you don't wish to make up your own. This program must run entirely in the console.
-     */
+
+public class MadLib{
+
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        String Noun1;
+        String Noun2;
+        String Noun3;
+        String occupation;
+        String verb;
+
+
+        System.out.println("Enter a noun");
+        Noun1 = read.nextLine();
+        System.out.println("Enter another noun ");
+        Noun2 = read.nextLine();
+        System.out.println("Enter one last noun ");
+        Noun3 = read.nextLine();
+        System.out.println("Enter an occupation ");
+        occupation = read.nextLine();
+        System.out.println("Enter a verb");
+        verb = read.nextLine();
+
+        System.out.println("It was during the battle of " + Noun1 + " When i was running through a " + Noun2 + " When a " + Noun3 + " went off right next to my platoon. our " + occupation + " yelled for us to " + verb);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
--- src/Sales.java
+++ src/Sales.java
@@ -1,6 +1,40 @@
+import javax.swing.JOptionPane;
+
 public class Sales {
 
-    /*Write a program, using dialog boxes, which prompts the user to enter a quantity, price, discount(as an integer) and sales tax(as an integer).
+
+
+
+    public static void main(String[] args) {
+     double Price;
+     int quantity;
+     double salesTax;
+     double discount;
+
+     Price = Integer.parseInt(JOptionPane.showInputDialog("What is the sale price?"));
+     quantity = Integer.parseInt(JOptionPane.showInputDialog("What is the quantity"));
+     salesTax = Integer.parseInt(JOptionPane.showInputDialog("What is the sales tax"));
+     discount = Integer.parseInt(JOptionPane.showInputDialog("What is the discount"));
+
+
+      double subtotal = Price * quantity;
+      double taxTotal = subtotal * salesTax/100.00;
+      double totalCost = subtotal + taxTotal;
+      double discountedPrice = subtotal * ( subtotal - discount);
+
+     JOptionPane.showMessageDialog(null, "subtotal "  + subtotal);
+     JOptionPane.showMessageDialog(null,"discounted price " + discountedPrice);
+     JOptionPane.showMessageDialog(null, "tax total "  + taxTotal);
+     JOptionPane.showMessageDialog(null, "totalCost " + totalCost);
+
+     System.exit(0);
+
+
+
+
+    }
+}
+/*Write a program, using dialog boxes, which prompts the user to enter a quantity, price, discount(as an integer) and sales tax(as an integer).
     The program should output the subtotal, tax total and total cost. Output should have proper currency format and proper rounding.
 
     Hint: You can use \n inside a String literal to make a new line, helpful for the dialog box output.
@@ -16,4 +50,3 @@ public class Sales {
     Tax Total: $1.57
     Total Cost: $24.03
      */
-}
